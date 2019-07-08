7ab2677c8c578e3e9ba1cf65ac8b2594b8163512
addinul kintangi <addins3009@gmail.com>
2019-07-09 01:00:00 +0700

    add initial swing ui

--- src/main/java/org/addin/learns/bt01/App.java
+++ src/main/java/org/addin/learns/bt01/App.java
@@ -1,13 +1,32 @@
 package org.addin.learns.bt01;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import org.addin.learns.bt01.ui.MenuUtama;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication(scanBasePackages = "org.addin.learns")
+public class App extends JFrame
 {
+    @Autowired
+    private MenuUtama menuUtama;
+    
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {        
+        var ctx = new SpringApplicationBuilder(App.class)
+                .headless(false)
+                .run(args);
+        
+        EventQueue.invokeLater(() -> {
+            var app = ctx.getBean(MenuUtama.class);
+            app.setVisible(true);
+        });
     }
 }
