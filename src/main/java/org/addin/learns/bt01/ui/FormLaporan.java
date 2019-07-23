9fb17a955ed7befb222b868afbcce7683ebb7e58
ahmad <dadan@klikpeta.com>
2019-07-23 16:19:01 +0700

    make empty table when no data

--- src/main/java/org/addin/learns/bt01/ui/FormLaporan.java
+++ src/main/java/org/addin/learns/bt01/ui/FormLaporan.java
@@ -75,13 +75,10 @@ public class FormLaporan extends javax.swing.JFrame {
 
         tablePembayaran.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+
             }
         ));
         jScrollPane2.setViewportView(tablePembayaran);
